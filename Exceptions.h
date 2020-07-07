00b3a9f92683506b5c12bd4511eebf7dbcfa992d
Rooklyn <61075163+rkline2@users.noreply.github.com>
2020-07-07 12:23:18 -0400

    version 2.0

---
+++ Exceptions.h
@@ -0,0 +1,39 @@
+/**************************************************************
+ * File:    MyExceptions.h
+ * Project: CMSC 341 - Project 3 - Word Freq Splay Tree
+ * Author :
+ * Date   : 16-July-2020
+ * Section: Lecture-02
+ * E-mail: rkline2@umbc.edu
+ *
+ * MyExceptions Class definition.
+ *
+ *************************************************************/
+#ifndef MYEXCEPTIONS_H
+#define MYEXCEPTIONS_H
+#include <string>
+
+class Exceptions {
+public:
+    /**********************************************************************
+     * Name: MyExceptions (Constructor)
+     * PreCondition: String containing error description.
+     *
+     * PostCondition:  Error object
+     *********************************************************************/
+    Exceptions(std::string message) : m_message(message) {}
+
+    /**********************************************************************
+     * Name: GetMessage
+     * PreCondition: None.
+     *
+     * PostCondition:  Returns error message.
+     *********************************************************************/
+    const std::string GetMessage() { return m_message; }
+
+private:
+
+    std::string m_message; //Error message.
+};
+
+#endif

b7652ce0c4ccf4a62de7f4e231fed47e67e14bd1
Rooklyn <61075163+rkline2@users.noreply.github.com>
2020-07-07 12:34:36 -0400

    version 2.0

--- Util.cpp
+++ Util.cpp
@@ -1,19 +1,19 @@
 /**************************************************************
  * File:    Util.cpp
  * Project: CMSC 341 - Project 3 - Word Frequency
- * Author : 
+ * Author :
  * Date   : 04-November-2014
  * Section: Lecture-02
- * E-mail:  
+ * E-mail:
  *
  * Util Class implementation.
  *
  * This class provides some support functions to other classes.
- * 
+ *
  * Lower() - Returns a string in lower case.
  * Strip() - Removes all nonalpha characters except ' and -
  * FileExisits() - Verifes a file exists.
- * 
+ *
  *************************************************************/
 #include "Util.h"
 
@@ -34,48 +34,49 @@ string Util::Lower(string inString)
 string Util::Strip(string inString)
 {
     std::string newString;
-    
+
     if (inString == "") return inString;
-    
+
     else if (!isalpha(inString[0])) {
         for (unsigned int i = 1; i < inString.length(); i++) {
             newString += inString[i];
         }
         return Strip(newString);
     }
-    
+
     else if (!isalpha(inString[inString.length() - 1])) {
         for (unsigned int i = 0; i < inString.length() - 1; i++) {
             newString += inString[i];
         }
         return Strip(newString);
     }
-    
+
     bool contraction = false;
     bool foundalpha = 0;
-    
+
     for (unsigned int i = 0; i < inString.length(); i++)
     {
         if (isalpha(inString[i])) {
             newString += inString[i];
             foundalpha = true;
-        } else if ((int(inString[i]) == 39 || int(inString[i]) == 45) && 
+        }
+        else if ((int(inString[i]) == 39 || int(inString[i]) == 45) &&
             !contraction && foundalpha) {
             newString += inString[i];
             contraction = true;
         }
     }
-    
-    
+
+
     return newString;
-    
+
 }
 
 
 bool Util::FileExists(const char* filename)
 {
     ifstream inFile(filename);
-    
+
     if (!inFile) {
         return false;
     }
